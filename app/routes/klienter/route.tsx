a3e13aab2a1176f7c7bdd50b5778ddc1cb8ea01b
Anum Qudsia <anum.qudsia@novari.no>
2024-06-24 13:25:34 +0200

    Add routes klienter og ressurser

---
+++ app/routes/klienter/route.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Klienter" },
+    { name: "description", content: "Liste over klienter" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">Velkomment til klienter :)</h1>
+    </div>
+  );
+}
