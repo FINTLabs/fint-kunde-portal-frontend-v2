144bb86316e8bf034d6bb1ce694c8887e587e158
Anum Qudsia <anum.qudsia@novari.no>
2024-07-03 19:17:30 +0200

    Add adapterList.json for offline development. Add route adapter detail. Handle navigation between adapter detail and adapter list

--- app/routes/adaptere/route.tsx
+++ app/routes/adaptere/route.tsx
@@ -2,7 +2,7 @@ import { json, type LoaderFunction, type MetaFunction } from '@remix-run/node';
 import InternalPageHeader from '~/components/shared/InternalPageHeader';
 import Breadcrumbs from '~/components/shared/breadcrumbs';
 import { MigrationIcon } from '@navikt/aksel-icons';
-import { BodyLong, Box, HStack, Label, Tabs, VStack } from '@navikt/ds-react';
+import { Box, Tabs } from '@navikt/ds-react';
 import { CogRotationIcon } from '@navikt/aksel-icons';
 import { NotePencilDashIcon } from '@navikt/aksel-icons';
 import { log } from '~/utils/logger';
@@ -10,8 +10,12 @@ import { getSession } from '~/utils/session';
 import AdapterAPI from '~/api/AdapterApi';
 import { useLoaderData } from '@remix-run/react';
 import { IAdapter } from '~/types/types';
-import { ChevronRightIcon } from '@navikt/aksel-icons';
 import { useState } from 'react';
+import adapters from '~/routes/adaptere/adapterList.json';
+import { tabInfo } from './constants';
+import { AdapterList } from './AdapterList';
+import { AdapterDetail } from '../adapter.$name/AdapterDetail';
+import { ArrowLeftIcon } from '@navikt/aksel-icons';
 
 interface IPageLoaderData {
     adapters?: IAdapter[];
@@ -29,103 +33,37 @@ export const loader: LoaderFunction = async ({ request }) => {
         const userSession = session.get('user-session');
 
         const adapters = await AdapterAPI.getAdapters(userSession.selectedOrganization.name);
+
         return json({ adapters });
     } catch (error) {
         console.error('Error fetching data:', error);
+
+        return json({ adapters });
+
         throw new Response('Not Found', { status: 404 });
     }
 };
 
-function ListItem({ adapter, onClick }: { adapter: IAdapter; onClick: () => void }) {
-    return (
-        <Box
-            onClick={onClick}
-            className="hover:bg-[--a-surface-subtle] active:bg-[--a-surface-active] hover:cursor-pointer"
-            borderRadius="large"
-            shadow="xsmall"
-            background="surface-transparent"
-            padding="6">
-            <HStack className="!flex !justify-between">
-                <VStack>
-                    <Label>{adapter.shortDescription}</Label>
-                    <BodyLong>{adapter.name}</BodyLong>
-                </VStack>
-                <VStack className="!flex !justify-center pr-3">
-                    <ChevronRightIcon title="Høyre peker" fontSize="1.7rem" />
-                </VStack>
-            </HStack>
-        </Box>
-    );
-}
-
-function AdapterList({
-    items,
-    onItemClick,
-}: {
-    items: IAdapter[];
-    onItemClick: (adapter: IAdapter) => void;
-}) {
-    return (
-        <VStack gap="5">
-            {items.map((adapter, index) => (
-                <ListItem key={index} adapter={adapter} onClick={() => onItemClick(adapter)} />
-            ))}
-        </VStack>
-    );
-}
-
-function AdapterDetail({ adapter }: { adapter: IAdapter }) {
-    return (
-        <Box padding="6" borderRadius="large" shadow="small" background="surface-subtle">
-            <HStack>
-                <Label>Client ID:</Label>
-                <BodyLong>{adapter.clientId}</BodyLong>
-            </HStack>
-            <HStack>
-                <Label>Note:</Label>
-                <BodyLong>{adapter.note}</BodyLong>
-            </HStack>
-            <HStack>
-                <Label>Assets:</Label>
-                <BodyLong>{adapter.assets.join(', ')}</BodyLong>
-            </HStack>
-            {/* Add more fields as needed */}
-        </Box>
-    );
-}
-
 function Tab({ value, adapters }: { value: string; adapters: IAdapter[] }) {
-    const [selectedAdapter, setSelectedAdapter] = useState<IAdapter | null>(null);
-
     return (
         <Tabs.Panel value={value} className="w-full p-10">
-            {selectedAdapter && <AdapterDetail adapter={selectedAdapter} />}
-            {!selectedAdapter && <AdapterList items={adapters} onItemClick={setSelectedAdapter} />}
+            <AdapterList items={adapters} />
         </Tabs.Panel>
     );
 }
+
 export default function Index() {
-    const breadcrumbs = [{ name: 'Adapter', link: '/adapter' }];
+    const breadcrumbs = [{ name: 'Adaptere', link: '/adaptere' }];
 
     const { adapters } = useLoaderData<IPageLoaderData>();
 
-    const tabInfo = [
-        {
-            value: 'manuelt-opprettet',
-            label: 'Manuelt opprettet',
-        },
-        {
-            value: 'automatisk-opprettet',
-            label: 'Automatisk opprettet',
-        },
-    ];
     return (
         <>
             <Breadcrumbs breadcrumbs={breadcrumbs} />
             <InternalPageHeader
-                title={'Adapter'}
+                title={'Adaptere'}
                 icon={MigrationIcon}
-                helpText="adapter"
+                helpText="adaptere"
                 hideBorder={true}
             />
 
@@ -150,6 +88,7 @@ export default function Index() {
                     </Tabs.List>
                     {tabInfo.map((tab, index) => (
                         <Tab
+                            key={index}
                             value={tab.value}
                             adapters={
                                 index === 1
