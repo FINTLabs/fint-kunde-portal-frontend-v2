5c7d4be25ebb3a275ff6c6c7b70d339db67de378
Anum Qudsia <hello@anum.io>
2024-07-16 17:16:53 +0200

    Clean up

--- app/routes/adapter.$name.update/route.tsx
+++ app/routes/adapter.$name.update/route.tsx
@@ -3,20 +3,18 @@ import AdapterAPI from '~/api/AdapterApi';
 import { getSelectedOprganization } from '~/utils/selectedOrganization';
 
 export const action = async ({ request, params }: ActionFunctionArgs) => {
-    console.log('Called update');
     const formData = await request.formData();
-
     const name = params.name;
     const shortDescription = formData.get('shortDescription');
     const note = formData.get('note');
 
-    console.log(formData);
-
-    if (!name) throw new Response('Failed to update. No name in params', { status: 404 });
+    if (!name)
+        throw new Response('Failed to update. Invalid name in request params', { status: 400 });
     if (!shortDescription)
-        throw new Response('Failed to update. No shortDescription in form data', { status: 404 });
-    if (!shortDescription)
-        throw new Response('Failed to update. No shortDescription in form data', { status: 404 });
+        throw new Response('Failed to update. Invalid shortDescriptio in formData', {
+            status: 400,
+        });
+    if (!note) throw new Response('Failed to update. Invalid note in formData', { status: 400 });
 
     const orgName = await getSelectedOprganization(request);
     const res = await AdapterAPI.updateAdapter(
@@ -28,7 +26,5 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
         orgName
     );
 
-    console.log('response in update');
-    console.log(res);
     return redirect(`/adapter/${params.name}`);
 };
