8dd8006482dc3f5761e72caa9ae3c7f1ee3612a2
Anum Qudsia <hello@anum.io>
2024-07-16 11:33:23 +0200

    Move fetchers values in a different layout than the static values

--- app/routes/adapter.$name/ValueDisplayPanel.tsx
+++ app/routes/adapter.$name/ValueDisplayPanel.tsx
@@ -1,4 +1,11 @@
-import { BodyShort, Button, CopyButton, HStack, Label } from '@navikt/ds-react';
+import {
+    BodyShort,
+    Button,
+    CopyButton as NavCopyButton,
+    HStack,
+    Label,
+    VStack,
+} from '@navikt/ds-react';
 import { ThumbUpIcon, ArrowCirclepathIcon } from '@navikt/aksel-icons';
 import { useFetcher } from '@remix-run/react';
 
@@ -13,31 +20,56 @@ export function ValueDisplayPanel({
 }) {
     const fetcher = useFetcher({ key: fetcherKey });
 
-    return (
-        <HStack className="flex !justify-between !items-center">
+    const Fetcher = () => {
+        return (
+            fetcherKey && (
+                <fetcher.Form method="post">
+                    <input type="hidden" name="type" value={label} />
+                    <Button
+                        type="submit"
+                        variant="tertiary-neutral"
+                        icon={<ArrowCirclepathIcon title="Refresh" fontSize="1.5rem" />}>
+                        Hent {label}
+                    </Button>
+                </fetcher.Form>
+            )
+        );
+    };
+
+    const CopyButton = () => {
+        return (
+            !!value && (
+                <NavCopyButton
+                    copyText={value}
+                    activeText={`${label} er kopiert!`}
+                    activeIcon={<ThumbUpIcon aria-hidden />}
+                />
+            )
+        );
+    };
+
+    return label === 'Klient Hemmelighet' || label === 'Passord' ? (
+        <VStack className="mt-2">
+            <HStack align={'center'} justify={'space-between'}>
+                <Label>{label}</Label>
+                <HStack>
+                    <Fetcher />
+                </HStack>
+            </HStack>
+            <HStack align="center" justify={'space-between'}>
+                <BodyShort>{value}</BodyShort>
+                <CopyButton />
+            </HStack>
+        </VStack>
+    ) : (
+        <HStack className="flex !justify-between !items-center mt-2">
             <HStack gap="4">
                 <Label>{label}</Label>
                 <BodyShort>{value}</BodyShort>
             </HStack>
-            <HStack className=" flex !items-center">
-                {fetcherKey && (
-                    <fetcher.Form method="post">
-                        <input type="hidden" name="type" value={label} />
-                        <Button
-                            type="submit"
-                            variant="tertiary-neutral"
-                            icon={<ArrowCirclepathIcon title="Refresh" fontSize="1.5rem" />}>
-                            Hent {label}
-                        </Button>
-                    </fetcher.Form>
-                )}
-                {!!value && (
-                    <CopyButton
-                        copyText={value}
-                        activeText={`${label} er kopiert!`}
-                        activeIcon={<ThumbUpIcon aria-hidden />}
-                    />
-                )}
+            <HStack className="flex !items-center">
+                <Fetcher />
+                <CopyButton />
             </HStack>
         </HStack>
     );
