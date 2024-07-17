f4c220bb84972d619c8073af381e01ac9a3fb60c
Anum Qudsia <hello@anum.io>
2024-07-17 14:00:12 +0200

    Display ressurser list in a table

--- app/routes/ressurser._index/route.tsx
+++ app/routes/ressurser._index/route.tsx
@@ -1,12 +1,13 @@
 import { type LoaderFunction, type MetaFunction } from '@remix-run/node';
 import { LayersIcon } from '@navikt/aksel-icons';
 import React from 'react';
-import { json, useLoaderData } from '@remix-run/react';
+import { json, useLoaderData, useNavigate } from '@remix-run/react';
 import Breadcrumbs from '~/components/shared/breadcrumbs';
 import InternalPageHeader from '~/components/shared/InternalPageHeader';
 import AccessApi from '~/api/AssetApi';
 import { IAsset } from '~/types/Asset';
 import { getSelectedOprganization } from '~/utils/selectedOrganization';
+import { Table } from '@navikt/ds-react';
 
 export const meta: MetaFunction = () => {
     return [
@@ -30,17 +31,36 @@ export default function Index() {
     const breadcrumbs = [{ name: 'ressurser', link: '/ressurser._index' }];
     const assets = useLoaderData<IAsset[]>();
     console.log(assets);
+
+    const navigate = useNavigate();
+
+    const handleClick = (id: string) => {
+        navigate(`/ressurser/${id}`);
+    };
     return (
         <>
             <Breadcrumbs breadcrumbs={breadcrumbs} />
             <InternalPageHeader title={'Ressurser'} icon={LayersIcon} helpText="assets" />
 
-            {assets.map((asset) => (
-                <div key={asset.dn}>
-                    <h2>{asset.name}</h2>
-                    <p>{asset.description}</p>
-                </div>
-            ))}
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell scope="col">Navn</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Beskrivelse</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {assets.map((item, i) => (
+                        <Table.Row
+                            key={i + item.dn}
+                            className="active:bg-[--a-surface-active] hover:cursor-pointer"
+                            onClick={() => handleClick(item.name)}>
+                            <Table.DataCell scope="row">{item.name}</Table.DataCell>
+                            <Table.DataCell scope="row">{item.description}</Table.DataCell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table>
         </>
     );
 }
