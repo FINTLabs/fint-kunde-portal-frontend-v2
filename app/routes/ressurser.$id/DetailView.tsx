894b1326113ddda890ccf84ec5e44e48fceea5e3
Anum Qudsia <hello@anum.io>
2024-07-17 16:07:43 +0200

    Make Edit Save Button Reusable. Make LabelValuePanel a reusable component. Re-use these both in resources detail

---
+++ app/routes/ressurser.$id/DetailView.tsx
@@ -0,0 +1,103 @@
+import { VStack, HStack, Heading, Button, Label, BodyLong } from '@navikt/ds-react';
+import { useSubmit } from '@remix-run/react';
+import { useState } from 'react';
+import { IAdapter } from '~/types/types';
+import { EditableTextField } from '../../components/shared/EditableTextField';
+import { PencilIcon, FloppydiskIcon } from '@navikt/aksel-icons';
+import { IAsset } from '~/types/Asset';
+import { ValueDisplayPanel } from '../adapter.$name/ValueDisplayPanel';
+import { LabelValuePanel } from '~/components/shared/LabelValuePanel';
+import { ToggleEditSaveButton } from '~/components/shared/EditSaveButton';
+
+export function DetailView({ asset }: { asset: IAsset }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [description, setDescription] = useState(asset.description);
+
+    const submit = useSubmit();
+
+    const toggleEditSave = () => {
+        if (isEditing) {
+            if (description.trim() !== asset.description) {
+                submit(
+                    {
+                        description: description,
+                    },
+                    {
+                        method: 'POST',
+                        action: 'update',
+                        navigate: false,
+                    }
+                );
+            }
+        }
+        setIsEditing(!isEditing);
+    };
+
+    return (
+        <VStack gap="3">
+            <HStack justify={'space-between'} align={'center'}>
+                <Heading align="start" size="medium">
+                    Detaljer
+                </Heading>
+            </HStack>
+
+            <HStack justify={'space-between'} gap="8">
+                <VStack gap="6">
+                    <LabelValuePanel label="Ressurs Id" value={asset.assetId} />
+                    <LabelValuePanel label="Navn" value={asset.name} />
+                </VStack>
+
+                <HStack className="w-full bg-red-300" align={'end'} justify={'space-between'}>
+                    <EditableTextField
+                        label={'Beskrivelse'}
+                        value={description}
+                        isEditing={isEditing}
+                        setValue={setDescription}
+                    />
+                    <ToggleEditSaveButton isEditing={isEditing} onClick={toggleEditSave} />
+                </HStack>
+
+                {/* <EditableTextField
+                    label={'Note'}
+                    value={adapterNote}
+                    isEditing={isEditing}
+                    setValue={setAdapterNote}
+                />
+                <HStack className="w-full" align={'end'} justify={'end'}>
+                    <Button
+                        icon={
+                            isEditing ? (
+                                <FloppydiskIcon title="Lagre" />
+                            ) : (
+                                <PencilIcon title="Rediger" />
+                            )
+                        }
+                        variant="tertiary"
+                        type="submit"
+                        onClick={() => {
+                            if (isEditing) {
+                                if (
+                                    adapterNote.trim() !== adapter.note ||
+                                    adapterShortDesc.trim() !== adapter.shortDescription
+                                ) {
+                                    submit(
+                                        {
+                                            note: adapterNote,
+                                            shortDescription: adapterShortDesc,
+                                        },
+                                        {
+                                            method: 'POST',
+                                            action: 'update',
+                                            navigate: false,
+                                        }
+                                    );
+                                }
+                            }
+                            setIsEditing(!isEditing);
+                        }}
+                    />
+                </HStack> */}
+            </HStack>
+        </VStack>
+    );
+}
--- app/routes/ressurser.$id/route.tsx
+++ app/routes/ressurser.$id/route.tsx
@@ -1,7 +1,7 @@
 import { type LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { LayersIcon } from '@navikt/aksel-icons';
 import React from 'react';
-import { json, useLoaderData } from '@remix-run/react';
+import { json, useLoaderData, useNavigate } from '@remix-run/react';
 import Breadcrumbs from '~/components/shared/breadcrumbs';
 import InternalPageHeader from '~/components/shared/InternalPageHeader';
 import AssetApi from '~/api/AssetApi';
@@ -10,6 +10,13 @@ import { IAsset } from '~/types/Asset';
 import { getSession } from '~/utils/session';
 import { IUserSession } from '~/types/types';
 import { getSelectedOprganization } from '~/utils/selectedOrganization';
+import { Box, HStack, VStack, Button, Heading } from '@navikt/ds-react';
+import Divider from 'node_modules/@navikt/ds-react/esm/dropdown/Menu/Divider';
+import Autentisering from '~/components/shared/Autentisering';
+import { DeleteAdapter } from '../adapter.$name/DeleteAdapter';
+import ComponentsTable from '../komponenter._index/ComponentsTable';
+import { ArrowLeftIcon } from '@navikt/aksel-icons';
+import { DetailView } from './DetailView';
 
 export const meta: MetaFunction = () => {
     return [
@@ -34,6 +41,7 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
 export default function Index() {
     const breadcrumbs = [{ name: 'Komponenter', link: '/komponenter._index' }];
     const assetData = useLoaderData<IAsset>();
+    const navigate = useNavigate();
 
     console.log(assetData);
     return (
@@ -41,6 +49,24 @@ export default function Index() {
             <Breadcrumbs breadcrumbs={breadcrumbs} />
             <InternalPageHeader title={'Ressurser'} icon={LayersIcon} helpText="assets" />
             {/* <ClientTable clients={assetData.clients} /> */}
+
+            <Box padding={'2'}>
+                <HStack>
+                    <VStack>
+                        <Button
+                            icon={<ArrowLeftIcon title="a11y-title" fontSize="1.5rem" />}
+                            variant="tertiary"
+                            onClick={() => navigate(`/adaptere`)}></Button>
+                    </VStack>
+                    <VStack className="flex flex-grow">
+                        <Box className="w-full" padding="6" borderRadius="large" shadow="small">
+                            <VStack gap="5">
+                                <DetailView asset={assetData} />
+                            </VStack>
+                        </Box>
+                    </VStack>
+                </HStack>
+            </Box>
         </>
     );
 }
