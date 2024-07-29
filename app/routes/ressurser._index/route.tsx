b5ff8eb7a970a2e486b8f6e76c074440c07014c7
Anum Qudsia <hello@anum.io>
2024-07-29 16:12:33 +0200

    Clean up and fix bug in Breadcrump

--- app/routes/ressurser._index/route.tsx
+++ app/routes/ressurser._index/route.tsx
@@ -30,9 +30,8 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Index() {
-    const breadcrumbs = [{ name: 'ressurser', link: '/ressurser._index' }];
+    const breadcrumbs = [{ name: 'R essurser', link: '/ressurser._index' }];
     const assets = useLoaderData<IAsset[]>();
-    console.log(assets);
 
     const navigate = useNavigate();
 
