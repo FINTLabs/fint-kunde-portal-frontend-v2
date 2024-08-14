d9f614e568601574c8e546ba63ee983dedc00f20
Anum Qudsia <hello@anum.io>
2024-08-14 13:30:53 +0200

    Add data structure for Log type. Display more details of a hendelse

--- app/routes/hendelseslogg/HealthStatusTable.tsx
+++ app/routes/hendelseslogg/HealthStatusTable.tsx
@@ -1,40 +1,37 @@
 import React from 'react';
 import { Table, BodyShort, Label } from '@navikt/ds-react';
+import { Log } from '~/types/types';
 
-interface TestResultsTableProps {
-    logResults: any[] | null; // Replace 'any[]' with the appropriate type for your log results
+interface HealthStatusProps {
+    logResults: Log[]; // TODO: Replace 'any[]' with the appropriate type for your log results
 }
 
-const HealthStatusTable: React.FC<TestResultsTableProps> = ({ logResults }) => {
+const HealthStatusTable: React.FC<HealthStatusProps> = ({ logResults }) => {
     return (
         <>
-            {logResults ? (
-                <>
-                    <Label>Helsestatus</Label>
-                    <Table size="small">
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell scope="col">Status</Table.HeaderCell>
-                                <Table.HeaderCell scope="col">Component</Table.HeaderCell>
-                                <Table.HeaderCell scope="col">Time</Table.HeaderCell>
+            <>
+                <Label>Helsestatus</Label>
+                <Table size="small">
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell scope="col">Status</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Component</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Time</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Type</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {logResults.map((log, index) => (
+                            <Table.Row key={index}>
+                                <Table.DataCell>{log.event.status}</Table.DataCell>
+                                <Table.DataCell>{log.event.corrId}</Table.DataCell>
+                                <Table.DataCell>{log.event.time}</Table.DataCell>
+                                <Table.DataCell>{log.event.action}</Table.DataCell>
                             </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            {logResults.map((result, index) => (
-                                <Table.Row key={index}>
-                                    <Table.DataCell>{result.tid}</Table.DataCell>
-                                    <Table.DataCell>{result.status}</Table.DataCell>
-                                    <Table.DataCell>{result.env}</Table.DataCell>
-                                </Table.Row>
-                            ))}
-                        </Table.Body>
-                    </Table>
-                </>
-            ) : (
-                <div>
-                    <BodyShort>please use the form to create a report</BodyShort>
-                </div>
-            )}
+                        ))}
+                    </Table.Body>
+                </Table>
+            </>
         </>
     );
 };
