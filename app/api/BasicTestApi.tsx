b291c08767e488f31f422f5b83e6478ecee302df
Hknots <hknots99@gmail.com>
2024-10-09 11:05:42 +0200

    Revert "add test runner ingress"
    This reverts commit a7c32b09e605488d297f4c4801a8addcc53a560d.

--- app/api/BasicTestApi.tsx
+++ app/api/BasicTestApi.tsx
@@ -1,7 +1,6 @@
 import { request } from '~/api/shared/api'; // Assuming this is your request helper function
 import { IBasisTest } from '~/types/BasisTest';
-
-const API_URL = process.env.TEST_RUNNER_API_URL
+import { API_URL } from '~/api/constants';
 
 class LogApi {
     static async runTest(orgName: string, status: string, component: string, time: string) {
@@ -11,7 +10,7 @@ class LogApi {
             time,
         };
 
-        const URL = `${API_URL}/test-runner/${orgName}/basic`;
+        const URL = `${API_URL}/api/tests/${orgName}/basic`;
         const functionName = 'runBasicTest';
 
         return await request(URL, functionName, 'POST', 'json', testBody);
