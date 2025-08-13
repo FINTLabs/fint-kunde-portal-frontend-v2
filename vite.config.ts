b4a97d8df7915ae2596132f672e7101e86829836
jennifer-fintlabs <jennifer@fintlabs.no>
2025-08-13 09:31:52 +0200

    upgrade tailwind 🐰

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         remix({
             future: {
                 v3_fetcherPersist: true,
