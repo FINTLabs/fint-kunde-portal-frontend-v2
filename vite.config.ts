ec8c49a0e5cd0802e19b9f76e7ea7badf3283748
jennifer-fintlabs <jennifer@fintlabs.no>
2026-06-22 14:49:46 +0200

    update all packages 🐰

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [tailwindcss(), !process.env.VITEST && reactRouter(), tsconfigPaths()].filter(Boolean),
+    plugins: [tailwindcss(), !process.env.VITEST && reactRouter()].filter(Boolean),
+    resolve: {
+        tsconfigPaths: true,
+    },
     server: {
         port: 3000,
         hmr: {
